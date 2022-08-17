87554974ec7a85b431be68b67f3d820cf4a25694
Robert <sordess@yahoo.ca>
2022-08-16 20:24:24 -0400

    FIX - Issue with negative values on Spectrum view

--- src/components/ui/voicing.js
+++ src/components/ui/voicing.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Accordion } from "react-bootstrap";
 import { useInstrumentContext } from "../utils/instrumentContext";
-import { FractionRangeParameterViewController, RangeParameterViewController } from "./inputs";
+import { FractionRangeParameterViewController, NegativeRangeParameterViewController, RangeParameterViewController } from "./inputs";
 import * as PtqApi from "../api/pqtApi";
 
 export const VoicingTabView = () => {
@@ -50,14 +50,14 @@ const HammerSettings = ({ context: ctx, reducer }) => {
 const SpectrumProfile = ({ context: ctx, reducer }) => {
     return (
         <>
-            <RangeParameterViewController label="Profile #1" name="profile1" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile1} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
-            <RangeParameterViewController label="Profile #2" name="profile2" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile2} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
-            <RangeParameterViewController label="Profile #3" name="profile3" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile3} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
-            <RangeParameterViewController label="Profile #4" name="profile4" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile4} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
-            <RangeParameterViewController label="Profile #5" name="profile5" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile5} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
-            <RangeParameterViewController label="Profile #6" name="profile6" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile6} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
-            <RangeParameterViewController label="Profile #7" name="profile7" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile7} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
-            <RangeParameterViewController label="Profile #8" name="profile8" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile8} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #1" name="profile1" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile1} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #2" name="profile2" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile2} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #3" name="profile3" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile3} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #4" name="profile4" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile4} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #5" name="profile5" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile5} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #6" name="profile6" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile6} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #7" name="profile7" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile7} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
+            <NegativeRangeParameterViewController label="Profile #8" name="profile8" min={-15} max={15} step={0.01} param={ctx.instrumentParameters.spectrumProfile8} dispatch={reducer} apiCallback={PtqApi.setParameterAsText} />
         </>
     )
 }
