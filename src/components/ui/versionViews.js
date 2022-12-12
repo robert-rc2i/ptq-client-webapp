46932b0bc24284151a9acb8b82d840504f0f9039
Robert <sordess@yahoo.ca>
2022-12-12 10:11:15 -0500

    Versions
    * Display info about versions of both the web app and Pianoteq

--- src/components/ui/versionViews.js
+++ src/components/ui/versionViews.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useInstrumentContext } from '../utils/instrumentContext';
+import { RenderIfTrue } from '../utils/loading';
 
-export function AppVersionView({appVersion="1.0.0", apiVersion="7.5.3"}) {
+export function AppVersionView({ appVersion = "1.0.0", apiVersion = "7.5.3" }) {
+    const [ctx] = useInstrumentContext();
     return (
-        <div className="text-center mt-2">
-            <span className="text-muted">Version {appVersion}</span>
-        </div>
+        <>
+            <div className="d-flex justify-content-between">
+                <div className="text-muted">Web application version</div>
+                <div className="text-muted">{appVersion}</div>
+            </div>
+            <RenderIfTrue predicate={ctx.ptqInfo.version}>
+                <div className="d-flex justify-content-between">
+                    <div className="text-muted">Detected Pianoteq version</div>
+                    <div className="text-muted">{ctx.ptqInfo.version}</div>
+                </div>
+            </RenderIfTrue>
+        </>
     );
 }
