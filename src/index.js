fad028fbe21c5951a9920625b80293f5e62b8b5f
Dallmer <philipdallmer@googlemail.com>
2026-07-06 08:31:55 +0200

    pwa setup

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -10,6 +11,12 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// Register the service worker for installability + offline app-shell caching.
+// NOTE: service workers only run in a secure context (HTTPS or localhost).
+// Over plain http://<host>:3000 this safely no-ops; it activates automatically
+// if the app is ever served over HTTPS.
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
